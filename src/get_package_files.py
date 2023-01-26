0102622a68f209f089e23c23b3cc0010b88c9060
Anders Perrone <perr0372@ln1001.mangi.msi.umn.edu>
2023-01-25 19:44:42 -0600

    Add multithreading

--- src/get_package_files.py
+++ src/get_package_files.py
@@ -124,17 +124,18 @@ from timeit import default_timer
 from concurrent.futures import ThreadPoolExecutor
 import re
 
-START TIME = default_timer()
+START_TIME = default_timer()
 
 PACKAGE_ID = 1203969
 SIZE = 1000
-first_url = 'https://nda.nih.gov/api/package/{}/files?page={}&size={}'.format(package_id, 1, size)
+first_url = 'https://nda.nih.gov/api/package/{}/files?page={}&size={}'.format(PACKAGE_ID, 1, SIZE)
 data = get_data(first_url, ndar_username, ndar_password)
 last_url = data['_links']['last']['href']
-num_pages = extract_page_num(last_url)
-
 def extract_page_num(url):
     return int(re.findall('page=[0-9]+', url)[0].replace('page=', ''))
+num_pages = extract_page_num(last_url)
+
+output = [(first_url, data['results'])]
 
 def sync_request(session, i):
     url = 'https://nda.nih.gov/api/package/{}/files?page={}&size={}'.format(PACKAGE_ID, i, SIZE)
@@ -156,19 +157,19 @@ def start_sync_process():
     
 
 def async_request(session, i):
+    start_time = default_timer()
     url = 'https://nda.nih.gov/api/package/{}/files?page={}&size={}'.format(PACKAGE_ID, i, SIZE)
     with session.get(url, auth=HTTPBasicAuth(ndar_username, ndar_password)) as response:
         if response.status_code != 200:
-            #print("FAILURE::{0}".format(url))
             print("{0:<30} {1:>20}".format(i, "FAILURE"))
             return(url, None)
         data = response.json()
-        elapsed_time = default_timer() - START_TIME
+        elapsed_time = default_timer() - start_time
         completed_at = "{:5.2f}s".format(elapsed_time)
         print("{0:<30} {1:>20}".format(i, completed_at))
         return(url, data['results'])
     
-async def start_async_process(num_workers, num_pages, output=[]):
+async def start_async_process(num_workers, num_pages, output):
     print("{0:<30} {1:>20}".format("No", "Completed at"))
     with ThreadPoolExecutor(max_workers=num_workers) as executor:
         with requests.Session() as session:
@@ -180,24 +181,128 @@ async def start_async_process(num_workers, num_pages, output=[]):
                     async_request,
                     *(session, i)
                 )
-                for i in range(2, num_pages)
+                for i in range(1, num_pages)
             ]
-            for response in asyncio.gather(*tasks):
+            for response in await asyncio.gather(*tasks):
                 output.append(response)
     return output
 
 START_TIME = default_timer()
+n_pages = 20
+n_workers = 5
 loop = asyncio.get_event_loop()
-files = loop.run_until_complete(start_async_process(5, 10))
-#future = asyncio.ensure_future(start_async_process(5, 20))
-#files = loop.run_until_complete(future)
+future = asyncio.ensure_future(start_async_process(n_workers, n_pages, output))
+files = loop.run_until_complete(future)
+elapsed_time = default_timer() - START_TIME
+print('Total time for {0} pages: {1}'.format(n_pages, "{:5.2f}s".format(elapsed_time)))
+#files = loop.run_until_complete(start_async_process(5, 10, output))
+
 
 # Upper limit on API requests seems to be approximately 5 workers and size=1000
 
+from asyncio import Queue
+
+@asyncio.coroutine
+def async_request(i, work_queue):
+    task_start_time = default_timer()
+    url = 'https://nda.nih.gov/api/package/{}/files?page={}&size={}'.format(PACKAGE_ID, i, SIZE)
+    r = requests.get(url, auth=HTTPBasicAuth(ndar_username, ndar_password))
+    if response.status_code != 200:
+        print("{0:<30} {1:>20}".format(i, "FAILURE"))
+        return(url, None)
+    data = response.json()
+    elapsed_time = default_timer() - task_start_time
+    completed_at = "{:5.2f}s".format(elapsed_time)
+    print("{0:<30} {1:>20}".format(i, completed_at))
+    return(url, data['results'])
+
+queue = Queue()
+[queue.put_nowait(page) for page in range(2,10)]
+
+import logging
+from queue import Queue
+from threading import Thread
+from time import time
+
+SERVICE_NAME = 'nda-tools'
+NDAR_USERNAME = input('Enter your NIMH Data Archives username: ')
+try:
+    NDAR_PASSWORD = keyring.get_password(SERVICE_NAME, ndar_username)
+except:
+    NDAR_PASSWORD = getpass.getpass('Enter your NIMH Data Archives password: ')
+
+PACKAGE_ID = 1203969
+SIZE = 1250
+
+def threaded_request(q, results):
+    while not q.empty():
+        page, url = q.get()
+        try:
+            start_time = default_timer()
+            r = requests.get(url, auth=HTTPBasicAuth(NDAR_USERNAME, NDAR_PASSWORD))
+            data = r.json()
+            results[url] = data
+            elapsed_time = default_timer() - start_time
+            completed_at = "{:5.2f}s".format(elapsed_time)
+            print("{0:<30} {1:>20}".format(url, completed_at))
+        except:
+            results[url] = None
+            print("{0:<30} {1:>20}".format(url, "FAILURE"))
+        q.task_done()
+    return True
+
+q = Queue(maxsize=0)
+num_threads = 6
+
+results = {'https://nda.nih.gov/api/package/{}/files?page={}&size={}'.format(PACKAGE_ID, page, SIZE): None for page in range(1, num_pages)}
+
+# Load queue with urls indexed by page
+for page in range(1, 10):
+    q.put((page, 'https://nda.nih.gov/api/package/{}/files?page={}&size={}'.format(PACKAGE_ID, page, SIZE)))
+
+START_TIME = default_timer()
+
+for i in range(num_threads):
+    logging.debug('Starting thread ', i)
+    worker = Thread(target=threaded_request, args=(q, results))
+    worker.setDaemon(True)
+    worker.start()
+
+print("{0:<30} {1:>20}".format("Requested URL", "Completed at"))
+q.join()
+print('All tasks completed in {}s'.format(default_timer() - START_TIME))
 
 
 
 
+class DownloadWorker(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            page = self.queue.get()
+            try:
+                threaded_request(page)
+            finally:
+                self.queue.task_done()
+
+from threading import Thread
+
+queue = Queue()
+num_threads = 5
+for _ in range(num_threads):
+    #worker = DownloadWorker(queue)
+    worker = Thread(target=threaded_request, args=(queue,))
+    worker.daemon = True
+    worker.start()
+
+for i in range(1, num_pages):
+    logger.info('Queueing {}'.format(i))
+    queue.put(i)
+queue.join()
+logging.info('Completed in {')
 
 
 
